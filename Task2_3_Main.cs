58152d983ae48ae3a10bf0c038da3c9622f8e9b5
Omar Sanchez <120536092+omarh2o2022@users.noreply.github.com>
2023-11-09 12:15:39 -0600

    Create Task2_3_Main.cs

---
+++ Task2_3_Main.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            
+            Training originalTraining = new Training
+            {
+                TextDescription = "C# training management system"
+            };
+                          
+            originalTraining.Add(new Lecture { TextDescription = "Intro to C#", Topic = "Basics" });
+            originalTraining.Add(new PracticalLesson { TextDescription = "Hands-on C# Programming", TaskConditionLink = "task-link", SolutionLink = "solution-link" });
+
+            Training clonedTraining = originalTraining.Clone();
+               
+            bool isPractical = clonedTraining.IsPractical();
+
+            Console.WriteLine($"Is practical: {isPractical}");
+            Console.Read();
+        }
+    }
+
+    
+}
