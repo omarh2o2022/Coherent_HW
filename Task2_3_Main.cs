0134db61a60726dd1560ddd23e8ed0331523d25d
Omar Sanchez <120536092+omarh2o2022@users.noreply.github.com>
2023-11-15 01:44:41 -0600

    Update Task2_3_Main.cs

--- Task2_3_Main.cs
+++ Task2_3_Main.cs
@@ -6,26 +6,22 @@ using System.Threading.Tasks;
 
 namespace Task2_3
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
-        {           
-            Training originalTraining = new Training
-            {
-                TextDescription = "C# training management system"
-            };
-                          
-            originalTraining.Add(new Lecture { TextDescription = "Intro to C#", Topic = "Basics" });
-            originalTraining.Add(new PracticalLesson { TextDescription = "Hands-on C# Programming", TaskConditionLink = "task-link", SolutionLink = "solution-link" });
+        {
+            Training originalTraining = new Training();
+            originalTraining.Add(new Lecture { Description = "Introduction to C#", Topic = "Basics" });
+            originalTraining.Add(new PracticalLesson { Description = "Hands-on with C#", TaskLink = "task1", SolutionLink = "solution1" });
 
             Training clonedTraining = originalTraining.Clone();
-               
-            bool isPractical = clonedTraining.IsPractical();
 
-            Console.WriteLine($"Is practical: {isPractical}");
+            Console.WriteLine("Is original training practical? " + originalTraining.IsPractical()); 
+            Console.WriteLine("Is cloned training practical? " + clonedTraining.IsPractical()); 
+       
             Console.Read();
         }
     }
 
-    
+
 }
