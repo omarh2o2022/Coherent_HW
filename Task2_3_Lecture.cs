57891ba29454415e4f0431ebf754035ef5581530
Omar Sanchez <120536092+omarh2o2022@users.noreply.github.com>
2023-11-15 01:49:05 -0600

    Create Task2_3_Lecture.cs

---
+++ Task2_3_Lecture.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2_3
+{
+    public class Lecture : Lesson
+    {
+        public string Topic { get; set; }
+    }
+}
