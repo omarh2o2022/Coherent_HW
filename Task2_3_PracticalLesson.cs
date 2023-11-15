62a4f3fde977c48a95adcf53b1bfa21325947864
Omar Sanchez <120536092+omarh2o2022@users.noreply.github.com>
2023-11-15 01:47:42 -0600

    Create Task2_3_PracticalLesson.cs

---
+++ Task2_3_PracticalLesson.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2_3
+{
+    public class PracticalLesson : Lesson
+    {
+        public string TaskLink { get; set; }
+        public string SolutionLink { get; set; }
+    }
+}
