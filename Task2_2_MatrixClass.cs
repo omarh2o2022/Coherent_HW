568880157225449effda790cf7d2af3c468602d9
Omar Sanchez <120536092+omarh2o2022@users.noreply.github.com>
2023-11-15 23:56:31 -0600

    Update Task2_2_MatrixClass.cs

--- Task2_2_MatrixClass.cs
+++ Task2_2_MatrixClass.cs
@@ -86,23 +86,4 @@ namespace Task2_2
             return $"Diagonal Matrix ({Size}x{Size})";
         }
     }
-
-    public static class DiagonalMatrixExtensions
-    {
-        public static MatrixElementsDiagonal Add(this MatrixElementsDiagonal firstMatrix, MatrixElementsDiagonal secondMatrix)
-        {
-            int size = Math.Max(firstMatrix.Size, secondMatrix.Size);
-            int[] resultElements = new int[size];
-
-            for (int i = 0; i < size; i++)
-            {
-                int value1 = (i < firstMatrix.Size) ? firstMatrix[i, i] : 0;
-                int value2 = (i < secondMatrix.Size) ? secondMatrix[i, i] : 0;
-                resultElements[i] = value1 + value2;
-            }
-
-            return new MatrixElementsDiagonal(resultElements);
-        }
-    }
-
 }
