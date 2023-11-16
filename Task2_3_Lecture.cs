360aa4249cb738b5fcdc6299ff393bf7bd62b03c
Omar Sanchez <120536092+omarh2o2022@users.noreply.github.com>
2023-11-16 00:25:41 -0600

    Update Task2_3_Lecture.cs

--- Task2_3_Lecture.cs
+++ Task2_3_Lecture.cs
@@ -9,5 +9,13 @@ namespace Task2_3
     public class Lecture : Lesson
     {
         public string Topic { get; set; }
+        public override object Clone()
+        {
+            return new Lecture
+            {
+               Description = this.Description,
+               Topic = this.Topic
+            };
+        }
     }
 }
