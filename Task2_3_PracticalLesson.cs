547400ea9d738689ad6e867ca5a8238d60927ea2
Omar Sanchez <120536092+omarh2o2022@users.noreply.github.com>
2023-11-16 00:26:41 -0600

    Update Task2_3_PracticalLesson.cs

--- Task2_3_PracticalLesson.cs
+++ Task2_3_PracticalLesson.cs
@@ -10,5 +10,14 @@ namespace Task2_3
     {
         public string TaskLink { get; set; }
         public string SolutionLink { get; set; }
+        public override object Clone()
+        {
+            return new PracticalLesson
+            {
+                Description = this.Description,
+                TaskLink = this.TaskLink,   
+                SolutionLink = this.SolutionLink    
+            };
+        }
     }
 }
