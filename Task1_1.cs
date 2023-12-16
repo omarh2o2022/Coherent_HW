27beb2ac471a58b3299a9e23ea56c1f06015eb0e
Omar Sanchez <120536092+omarh2o2022@users.noreply.github.com>
2023-12-16 01:28:22 -0600

    Update Task1_1.cs

--- Task1_1.cs
+++ Task1_1.cs
@@ -1,9 +1,4 @@
 using System;
-using System.Collections.Generic;
-using System.Diagnostics.Eventing.Reader;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace proyect01_Omar
 {
