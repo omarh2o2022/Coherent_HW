9d1e711a72ae8067d39cc2a5c3cfe0f6934a9602
Omar Sanchez <120536092+omarh2o2022@users.noreply.github.com>
2023-12-29 03:01:57 -0600

    Update Task_Library.cs

--- Task_Library.cs
+++ Task_Library.cs
@@ -4,21 +4,27 @@ namespace Task6.task7
 {
     public class Library
     {
+        public Library() { } // For deserialization from data file
         public Catalog Catalog { get; set; }
         public List<string> PressReleaseItems { get; set; }
 
-        // Parameterless constructor
-        public Library()
-        {
-            Catalog = new Catalog(); // Initialize with an empty catalog
-            PressReleaseItems = new List<string>(); // Initialize with an empty list
-        }
-
-        // Constructor with parameters
         public Library(Catalog catalog, List<string> pressReleaseItems)
         {
             Catalog = catalog;
             PressReleaseItems = pressReleaseItems;
+
+            InitializePagesForEBooks();
+        }
+
+        private void InitializePagesForEBooks()
+        {
+            foreach (var book in Catalog.Books)
+            {
+                if (book is EBook eBook)
+                {
+                    eBook.FetchPagesAsync().Wait(); // Using Wait() for simplicity; consider using async all the way
+                }
+            }
         }
     }
 }
